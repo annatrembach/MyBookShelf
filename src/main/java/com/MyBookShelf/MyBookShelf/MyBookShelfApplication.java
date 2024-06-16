f115dd0bc616a0cc3912626894c46fd40cfcd277
Anna <trembach.ann@gmail.com>
2024-06-16 21:39:06 +0300

    Spring Boot

---
+++ src/main/java/com/MyBookShelf/MyBookShelf/MyBookShelfApplication.java
@@ -0,0 +1,13 @@
+package com.MyBookShelf.MyBookShelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyBookShelf/MyBookShelf/MyBookShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyBookShelf.MyBookShelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
